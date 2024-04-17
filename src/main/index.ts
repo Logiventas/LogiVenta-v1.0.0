89a18cd668da19a82ae9287ea797d1f3da37f660
Brayan Guevara <bguevaravan@gmail.com>
2024-04-17 10:34:29 -0500

    configuracion de servidor y cliente

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,6 +2,8 @@ import { app, shell, BrowserWindow, ipcMain } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
+import '../renderer/src/server/server'
+require('electron-reload')(__dirname);
 
 function createWindow(): void {
   // Create the browser window.
@@ -10,10 +12,11 @@ function createWindow(): void {
     height: 670,
     show: false,
     autoHideMenuBar: true,
-    ...(process.platform === 'linux' ? { icon } : {}),
+    ...(process.platform === 'linux' ? { icon } : undefined ),
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
-      sandbox: false
+      sandbox: false,
+      nodeIntegration: true
     }
   })
 
---
+++ src/renderer/src/server/server.ts
@@ -0,0 +1,28 @@
+import express, {  Response } from 'express'; // Uso de ES6 import
+
+const app = express();
+const port = process.env.PORT || 8080;  // Establece el puerto
+
+
+// Ejemplo de ajustes en las rutas
+app.get('/', (_, res: Response) => {
+  res.json({ mensaje: '¡Hola Mundo!' });
+});
+
+app.get('/cervezas', (_, res: Response) => {
+  res.json({ mensaje: '¡A beber cerveza!' });
+});
+
+app.post('/', (_, res: Response) => {
+  res.json({ mensaje: 'Método post' });
+});
+
+app.delete('/', (_, res: Response) => {
+  res.json({ mensaje: 'Método delete' });
+});
+
+
+
+app.listen(port, () => {
+  console.log(`API escuchando en el puerto ${port}`);
+});
