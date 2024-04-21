ae9c23964300ffcdb337b2855d96a110a615e1c9
Brayan Guevara <bguevaravan@gmail.com>
2024-04-21 03:56:04 -0500

    FronEnd:Loader

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -6,6 +6,7 @@ function App (): JSX.Element {
 <>
 
     <Router/>
+    
 
 </>
 
--- src/renderer/src/client/routes/router.tsx
+++ src/renderer/src/client/routes/router.tsx
@@ -1,31 +1,40 @@
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Principal from '../pages/inicioConfiguracion/Principal'
 import ConfigPrincipal from '../pages/inicioConfiguracion/ConfigPrincipal'
 import ConfigSecundaria from '../pages/inicioConfiguracion/configSecundaria'
 import Home from '../pages/home/Home'
 import IniciarSesion from '../pages/iniciarSesion/iniciarSesion'
-import  Config  from '../../../../../config.json'
+import Config from '../../../../../config.json'
 
-function AppRouter () {
 
-    const inicial = Config.server && Config.client?true:false
+function AppRouter() {
+    const inicial = Config.server && Config.client ? true : false;
 
-  return (
+    const [loading, setLoading] = useState(false);  // Estado para controlar la visibilidad del loader
 
+    // Función para ocultar el loader
+    const handleLoadComplete = (option:boolean) => {
+        option? setLoading(true): setLoading(false)    
+    }
+    useEffect(() =>{
+        handleLoadComplete(true)
+    })
+
+    return (
         <>
-
+        {   }
+            {loading?<div className="loader"></div>:''} 
             <Router>
                 <Routes>
-                    <Route path="/" element={inicial?<IniciarSesion />:<Principal />} />
-                    <Route path="/home" element={<Home />} />
-                    <Route path="/config1" element={<ConfigPrincipal />} />
-                    <Route path="/config2" element={<ConfigSecundaria />} />
+                    <Route path="/" element={inicial ? <IniciarSesion  /> : <Principal  />} />
+                    <Route path="/home" element={<Home  />} />
+                    <Route path="/config1" element={<ConfigPrincipal  />} />
+                    <Route path="/config2" element={<ConfigSecundaria  />} />
                 </Routes>
             </Router>
-
         </>
-
-  )
+    )
 }
 
-export default AppRouter
+export default AppRouter;
