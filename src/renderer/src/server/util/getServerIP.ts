57d4e9ea9ad3352040dc77d30567bfe747ec95d5
Brayan Guevara <bguevaravan@gmail.com>
2024-04-26 18:33:40 -0500

    BackEnd: optiene IP del servidor

---
+++ src/renderer/src/server/util/getServerIP.ts
@@ -0,0 +1,14 @@
+import os from "os";
+// Función para obtener la IP del servidor
+export default function getServerIP(): string {
+    const interfaces = os.networkInterfaces();
+    for (const name of Object.keys(interfaces)) {
+      for (const iface of interfaces[name]!) {
+        if (iface.family === "IPv4" && !iface.internal) {
+          return iface.address;
+        }
+      }
+    }
+    return "127.0.0.1"; // Retorno localhost como fallback
+  }
+
