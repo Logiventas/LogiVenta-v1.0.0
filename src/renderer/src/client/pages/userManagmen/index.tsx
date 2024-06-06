1b2018c63a7201b8e3e88db1530fadd593623b70
Brayan Guevara <bguevaravan@gmail.com>
2024-06-05 19:41:27 -0500

    solucion de error en el index de userManagmen

--- src/renderer/src/client/pages/userManagmen/index.tsx
+++ src/renderer/src/client/pages/userManagmen/index.tsx
@@ -1,33 +1,16 @@
 
 import { Outlet, useLocation } from 'react-router-dom';
 import Icono from "../../components/component_icono";
-
-import { Outlet, useLocation } from 'react-router-dom';
-import Icono from "../../components/component_icono";
 import icon_user from '@renderer/assets/icon/userManagmen.png';
-import icon_profile from '@renderer/assets/icon/userProfile.png';
 import icon_profile from '@renderer/assets/icon/userProfile.png';
 import BarraUsuario from "@renderer/client/components/barraUsuario/component_barraUsuario";
 
 const UserManagement = () => {
     const location = useLocation();
 
-
-const UserManagement = () => {
-    const location = useLocation();
-
     return (
         <>
-            <BarraUsuario />
-            <div className="d-flex w-100 h-75 align-content-center mx-auto my-5 justify-content-center row">
-                {/* Renderizar iconos solo si la ruta actual es exactamente /userManagement */}
-                {location.pathname === '/userManagement' && (
-                    <>
-                        <Icono enlace="/userManagement/users" modulo="Usuarios" urlImg={icon_user} />
-                        <Icono enlace="/userManagement/profiles" modulo="Perfiles de Usuario" urlImg={icon_profile} />
-                    </>
-                )}
-                <Outlet />
+ 
             <BarraUsuario />
             <div className="d-flex w-100 h-75 align-content-center mx-auto my-5 justify-content-center row">
                 {/* Renderizar iconos solo si la ruta actual es exactamente /userManagement */}
@@ -43,11 +26,4 @@ const UserManagement = () => {
     );
 };
 
-export default UserManagement;
-
-
-    );
-};
-
-export default UserManagement;
-
+export default UserManagement
