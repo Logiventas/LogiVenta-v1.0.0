5c1414490f4aa80c7ef03ea5b835eecab1069761
Brayan Guevara <bguevaravan@gmail.com>
2024-06-09 21:42:38 -0500

    CU_5.1 Usuarios

--- src/renderer/src/client/pages/userManagmen/index.tsx
+++ src/renderer/src/client/pages/userManagmen/index.tsx
@@ -1,9 +1,9 @@
 
 import { Outlet, useLocation } from 'react-router-dom';
-import Icono from "../../components/component_icono";
+import Icono from "../../components/Icon.component";
 import icon_user from '@renderer/assets/icon/userManagmen.png';
 import icon_profile from '@renderer/assets/icon/userProfile.png';
-import BarraUsuario from "@renderer/client/components/barraUsuario/component_barraUsuario";
+import BarraUsuario from "@renderer/client/components/userBar/index";
 
 const UserManagement = () => {
     const location = useLocation();
@@ -12,7 +12,7 @@ const UserManagement = () => {
         <>
  
             <BarraUsuario />
-            <div className="d-flex w-100 h-75 align-content-center mx-auto my-5 justify-content-center row">
+            <div className="d-flex w-100 align-content-center mx-auto my-1 justify-content-center row">
                 {/* Renderizar iconos solo si la ruta actual es exactamente /userManagement */}
                 {location.pathname === '/userManagement' && (
                     <>
