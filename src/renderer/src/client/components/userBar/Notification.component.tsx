9ef10f119412bd3a2480841057da6af2381072b1
Brayan Guevara <bguevaravan@gmail.com>
2024-06-17 01:25:07 -0500

    eliminación de  fragmento innecesario

--- src/renderer/src/client/components/userBar/Notification.component.tsx
+++ src/renderer/src/client/components/userBar/Notification.component.tsx
@@ -1,25 +1,25 @@
 
 import icon_notificacin from '/src/assets/icon/notificacion.png'
-const Notification =(prop:any)=>{
-    return(
-        <>
-                <button className="btn">
-                    <i className="fas fa-bell">
-                        <img
-                            style={{ width: "40px", height: "40px", marginRight: "10px" }}
-                            src={icon_notificacin}
-                            alt="Notificaciones"
-                        />
-                         <span   
-                            style={{ position:'absolute' ,transform:' translate(-25px,12px)'}} 
-                            className="badge rounded-pill badge-notification bg-danger">
-                            {prop.numero!=0?prop.numero:''}
-                        </span>
-   
-                    </i>
-                   
-                </button>
-        </>
+const Notification = (prop: any) => {
+    return (
+
+        <button className="btn">
+            <i className="fas fa-bell">
+                <img
+                    style={{ width: "40px", height: "40px", marginRight: "10px" }}
+                    src={icon_notificacin}
+                    alt="Notificaciones"
+                />
+                <span
+                    style={{ position: 'absolute', transform: ' translate(-25px,12px)' }}
+                    className="badge rounded-pill badge-notification bg-danger">
+                    {prop.numero != 0 ? prop.numero : ''}
+                </span>
+
+            </i>
+
+        </button>
+
     )
 }
-export  default Notification
+export default Notification
