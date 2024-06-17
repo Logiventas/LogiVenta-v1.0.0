f46ba840ecb4178af638e070ab70ee068a498338
Brayan Guevara <bguevaravan@gmail.com>
2024-06-17 12:55:12 -0500

    Correcion de parametros para base de datos por problema al desplegar

--- src/renderer/src/server/config/db.config.ts
+++ src/renderer/src/server/config/db.config.ts
@@ -1,18 +1,17 @@
 //src\renderer\src\server\config\db.config.ts
-import dotenv from 'dotenv';
-import { Sequelize } from 'sequelize';
 
-// Cargar las variables de entorno desde el archivo .env
+import dotenv from "dotenv";
+import { Sequelize } from "sequelize";
+// Cargar variables de entorno desde el archivo .env
 dotenv.config();
-
 const sequelize = new Sequelize(
-  process.env.DB_NAME as string,
-  process.env.DB_USER as string,
-  process.env.DB_PASSWORD as string,
+  (process.env.DB_NAME as string) ?? "dbpos",
+  (process.env.DB_USER as string) ?? "root",
+  (process.env.DB_PASSWORD as string) ?? "12345",
   {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT as 'mysql' | 'mariadb' | 'postgres' | 'mssql',
-    logging: console.log, // Aquí activas el logging
+    host: process.env.DB_HOST as string ?? "localhost",
+    dialect: process.env.DB_DIALECT as any ?? "mysql",
+    logging: console.log,
   }
 );
 
