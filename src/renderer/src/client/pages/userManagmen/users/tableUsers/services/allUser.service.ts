b301f2283f5d4992e461b67b55767e0435def0f2
Brayan Guevara <bguevaravan@gmail.com>
2024-06-19 00:36:46 -0500

    Cambio en el servicio y  interceptor

--- src/renderer/src/client/pages/userManagmen/users/tableUsers/services/allUser.service.ts
+++ src/renderer/src/client/pages/userManagmen/users/tableUsers/services/allUser.service.ts
@@ -1,5 +1,19 @@
 // src/renderer/src/client/pages/userManagmen/components/users/services/allUser.service.ts
+import config from '@renderer/config.json';
 import api from '../interceptor/allUser.interceptor';
+
+if (!config.IP_SERVER || !config.PORT_SERVER) {
+  
+  throw new Error('La configuración del servidor no está definida correctamente en config.json');
+}
+
+
+// Ajustar la configuración base de la instancia de axios
+api.defaults.baseURL = `http://${config.IP_SERVER}:${config.PORT_SERVER}/api`;
+api.defaults.headers.common['Content-Type'] = 'application/json';
+api.defaults.headers.common['Accept'] = 'application/json';
+api.defaults.headers["x-permission-code"] =  "GU01-00";
+api.defaults.timeout = 1000;
 
 const URL = '/users/allUsers';
 
