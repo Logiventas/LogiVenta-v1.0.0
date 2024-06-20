cb375c5ff3dc1884b3683bb0576f2ab07e5dbeb5
Brayan Guevara <bguevaravan@gmail.com>
2024-06-20 01:22:58 -0500

    Corrección para usar datos del context

--- src/renderer/src/client/pages/userManagmen/users/editUser/components/SelectorCountryCity.component.tsx
+++ src/renderer/src/client/pages/userManagmen/users/editUser/components/SelectorCountryCity.component.tsx
@@ -1,64 +1,75 @@
-import React, { useEffect, useState } from 'react';
-import InputSelect from "@client/components/InputSelect.component";
-import User from "../models/User.model";
+import React, { useEffect, useState, useContext } from 'react';
+import SelecteUserContext from '@client/contexts/userContext';
 
-// Definición de datos de países y ciudades con identificadores numéricos
-const countryCityData = {
-    2: { name: "Argentina", cities: { 2: "Buenos Aires", 3: "Córdoba" } },
-    3: { name: "Bolivia", cities: { 4: "La Paz" } },
-    4: { name: "Chile", cities: { 7: "Santiago" } }
-};
+const SelectorCountryCity = ({ user, handleChange }) => {
+  const { countries } = useContext(SelecteUserContext);
+  const [cities, setCities] = useState<string[]>([]);
 
-interface SelectorCountryCityProps {
-    user: User;
-    handleChange: (e: { target: { id: string, value: any } }) => void;
-}
+  useEffect(() => {
+    if (user.homeCountry) {
+      const selectedCountry = countries.find(country => country.id === user.homeCountry);
+      const cityNames:any = selectedCountry ? selectedCountry.cities.map(city => city.name).filter(city => city !== null) : [];
+      setCities(cityNames);
+    } else {
+      setCities([]);
+    }
+  }, [user.homeCountry, countries]);
 
-const SelectorCountryCity: React.FC<SelectorCountryCityProps> = ({ user, handleChange }) => {
-    const [cities, setCities] = useState<string[]>([]);
+  const handleCountryChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedCountryId = parseInt(e.target.value, 10);
+    handleChange({ target: { id: 'homeCountry', value: selectedCountryId } });
+    handleChange({ target: { id: 'homeCity', value: '' } }); // Restablecer ciudad a "Seleccione"
+    const selectedCountry = countries.find(country => country.id === selectedCountryId);
+    const cityNames:any = selectedCountry ? selectedCountry.cities.map(city => city.name).filter(city => city !== null) : [];
+    setCities(cityNames);
+  };
 
-    useEffect(() => {
-        if (user.homeCountry) {
-            const cityNames = Object.values<string>(countryCityData[user.homeCountry]?.cities || {});
-            setCities(cityNames);
-        } else {
-            setCities([]);
-        }
-    }, [user.homeCountry]);
+  const handleCityChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedCityName = e.target.value;
+    const selectedCountry = countries.find(country => country.id === user.homeCountry);
+    const selectedCityId = selectedCountry ? selectedCountry.cities.find(city => city.name === selectedCityName)?.id : null;
+    
+    if (selectedCityId !== null) {
+      handleChange({ target: { id: 'homeCity', value: selectedCityId } });
+    }
+  };
 
-    const handleCountryChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        const selectedCountryId = parseInt(e.target.value, 10);
-        handleChange({ target: { id: 'homeCountry', value: selectedCountryId } });
-        handleChange({ target: { id: 'homeCity', value: 0 } }); // Restablecer ciudad a "Seleccione"
-        const cityNames = Object.values<string>(countryCityData[selectedCountryId]?.cities || {});
-        setCities(cityNames);
-    };
-
-    const handleCityChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        const selectedCityId = parseInt(e.target.value, 10);
-        handleChange({ target: { id: 'homeCity', value: selectedCityId } });
-    };
-
-    return (
-        <div className='d-flex'>
-            <div className="me-2 col-6">
-                <label className="form-label" htmlFor="homeCountry">País</label>
-                <InputSelect 
-                    options={Object.values(countryCityData).map(country => country.name)} 
-                    value={countryCityData[user.homeCountry]?.name || ''} 
-                    onChange={handleCountryChange} 
-                />
-            </div>
-            <div className="col-6 ms-1 pe-3">
-                <label className="form-label" htmlFor="homeCity">Ciudad</label>
-                <InputSelect 
-                    options={cities} 
-                    value={cities.find(city => city === countryCityData[user.homeCountry]?.cities[user.homeCity]) || ''} 
-                    onChange={handleCityChange} 
-                />
-            </div>
-        </div>
-    );
+  return (
+    <div className='d-flex'>
+      <div className="me-2 col-6">
+        <label className="form-label" htmlFor="homeCountry">País</label>
+        <select
+          className="form-select"
+          value={user.homeCountry ? String(user.homeCountry) : ''}
+          onChange={handleCountryChange}
+        >
+          <option value="" disabled>Seleccione</option>
+          {countries
+            .filter(country => country.name !== null)
+            .map(country => (
+              <option key={country.id} value={country.id}>
+                {country.name}
+              </option>
+            ))}
+        </select>
+      </div>
+      <div className="col-6 ms-1 pe-3">
+        <label className="form-label" htmlFor="homeCity">Ciudad</label>
+        <select
+          className="form-select"
+          value={user.homeCity ? String(user.homeCity) : ''}
+          onChange={handleCityChange}
+        >
+          <option value="" disabled>Seleccione</option>
+          {cities.map((city, index) => (
+            <option key={index} value={city}>
+              {city}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
 };
 
 export { SelectorCountryCity };
