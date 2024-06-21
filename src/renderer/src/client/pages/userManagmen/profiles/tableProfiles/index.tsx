aaac4432c9022900d2ba96a36ff33b9639802421
Brayan Guevara <bguevaravan@gmail.com>
2024-06-21 14:01:37 -0500

    FrontEnd: CU_5.3 Perfiles de usuario

---
+++ src/renderer/src/client/pages/userManagmen/profiles/tableProfiles/index.tsx
@@ -0,0 +1,38 @@
+//src\renderer\src\client\pages\userManagmen\profiles\tableProfiles\index.tsx
+import { useContext } from "react";
+import SelecteUserContext from '@client/contexts/userContext';
+import { TituloModulo } from "@renderer/client/components/TitleModule.component";
+import icon_profiles from '@renderer/assets/icon/userProfile.png';
+import { Link } from "react-router-dom";
+import TableUsers from "./components/TableProfile.component";
+import TableFilter from "./components/TableProfileFilter.component";
+import { useProfileFilter } from './hooks/useProfileFilter.hooks';
+
+const Profiles = () => {
+    const { user } = useContext(SelecteUserContext);
+    const { filteredUsers, handleFilterChange } = useProfileFilter();
+
+    return (
+        <div style={{ width: '98%' }} className="m-auto">
+            <TituloModulo titulo="Perfiles de Usuario" img={icon_profiles} />
+            <div className="d-flex flex-column">
+                <div className="mt-1 p-2">
+                    <div className="ms-4">
+                        <h4>Perfiles</h4>
+                        <div className="d-flex gap-2 my-3">
+                            <TableFilter onFilterChange={handleFilterChange} />
+                            {user.access["GU01-01"] && (
+                                <Link to="/userManagement/newUser" className="btn btn-primary mb-1">Nuevo Perfil</Link>
+                            )}
+                        </div>
+                    </div>
+                    <div>
+                        <TableUsers data={filteredUsers} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Profiles;
