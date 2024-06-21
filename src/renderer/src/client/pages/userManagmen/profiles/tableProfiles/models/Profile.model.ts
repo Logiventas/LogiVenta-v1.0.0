abd910dfea6ecf9754a94a25d96210f3d2a78ad2
Brayan Guevara <bguevaravan@gmail.com>
2024-06-21 14:42:20 -0500

    Revert "FrontEnd: CU_5.3 Perfiles de usuario"
    This reverts commit aaac4432c9022900d2ba96a36ff33b9639802421.

--- src/renderer/src/client/pages/userManagmen/profiles/tableProfiles/models/Profile.model.ts
+++
@@ -1,8 +0,0 @@
-// src/renderer/src/client/pages/userManagmen/profiles/tableProfiles/models/Profile.model.ts
-interface Profile {
-    id:number;
-    profile: string;
-    description: string;
-}
-
-export type { Profile };
