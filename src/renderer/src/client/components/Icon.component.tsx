bb6cc109d86c7309f6b815fa1590b11b64961da7
Brayan Guevara <bguevaravan@gmail.com>
2024-07-02 12:22:11 -0500

    tamaño del alto fijo

--- src/renderer/src/client/components/Icon.component.tsx
+++ src/renderer/src/client/components/Icon.component.tsx
@@ -5,7 +5,7 @@ const Icono = (props: any) => {
   return (
         <Routes>
             <Route path='/' element={ // Corrección para usar la prop 'element'
-                <Link className='w-25 py-3' to={props.enlace}>
+                <Link className='w-25 h-25 py-3' to={props.enlace}>
                     <div style={{ width: '8rem', cursor: 'pointer' }} className="iconoHover d-flex flex-column align-items-center m-auto ">
                         <img className="" style={{ width: '75px' }} src={props.urlImg} alt="Imagen del módulo"></img>
                         <h6 className="text-center mt-3">{props.modulo}</h6>
